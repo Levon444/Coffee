0574c0347bb974b12e80ddc966ad3ec683fe3b41
Levon444 <levonbadasyan8@gmail.com>
2021-02-23 12:41:03 +0300

    Debug functions in source.js

--- source/scripts/source.js
+++ source/scripts/source.js
@@ -1,40 +1,59 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var but = document.getElementById('canv');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const but = document.getElementById('canv');
+const form = document.forms.vals;
 
-
-class Pre_Entropy {
-  constructor(){
   let n  /* Количество испытаний */
-  let p /*Вероятность конкретного исхода*/
+  let p;/*Вероятность конкретного исхода*/
   let k; //Предполагаемое количество появлений конкретного исхода во время испытаний
   let P; //Вероятность предполагаемого количества конкретных исходов в цикле испытаний
-  };
   //Факториал числа
-   Fact(a) {
-     for(var i=1; i <= n; i++){
-      a = a*(n-i);
-     }   
-     return a
+  function Fact(a) {
+     if (a==0){
+       return 1
+     }else{
+       return a * Fact(a-1)
+     }
   };
   //Фомула Бернулли для вероятности определенного количества испытаний
-  Bernulli(n,p,k){
+  function Bernulli(n,p,k){
     P = (this.Fact(n)/(this.Fact(k)*this.Fact(n-k))*Math.pow(p,k)*Math.pow(1-p,n-k));
     return P
+  };
+  but.onclick = function(){
+    n = form.elements.val_n.value;
+    p = form.elements.val_p.value;
+    n = Number(n);
+    p = Number(p)
+    let Probability = [];//Массив, содержащий вероятностную схему для данного события.
+    for(var j=0; j <= n; j++){
+      k = n-j;
+      prototype = Bernulli(n,p,k);
+      Probability.push(P);
+      console.log(Probability[j])
+    }
   }
-  
-}
-//Событие по нажатию кнопки
+   /* */
+    
+    
+
+/*Событие по нажатию кнопки
 but.onclick = function(){
-let entr = new.Pre_Entropy;
-entr.n = document.getElementById('n').value;
-entr.p = document.getElementById('p').value;
+let entr = new Pre_Entropy(
+n = document.getElementById('n').value,
+p = document.getElementById('p').value
+);
+n = Number(n);
+p = Number(p);
+console.log(p)
 let Probability = [];//Массив, содержащий вероятностную схему для данного события.
 for(var j=0; j <= n; j++){
   entr.k = n-j;
-  entr.Bernulli(n,p,k);
-  Probability.push(P)
+  entr.prototype = Bernulli(n,p,k);
+  Probability.push(P);
+  console.log(Probability[j])
 }
+;
 }
 /*but.onclick = function(){
 var elems = [];
